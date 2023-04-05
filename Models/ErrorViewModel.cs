0452d427aa49a7ef50835209da7be382cb2186c2
muratBee01 <muratalibek01@gmail.com>
2023-04-05 09:38:43 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitopiaQuest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
