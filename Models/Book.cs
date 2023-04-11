b68f4c7efa011d71a6ab690581a000661d5115a6
muratBee01 <muratalibek01@gmail.com>
2023-04-11 08:38:38 +0600

    Create, Read, Update, and Delete (CRUD) controller for the Book model.

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,19 @@
-﻿namespace DigitopiaQuest.Models
+﻿using System.ComponentModel;
+
+namespace DigitopiaQuest.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [DisplayName("Book Name")]
         public string NameOfBook { get; set; } = string.Empty;
+        [DisplayName("Book Description")]
         public string DescriptionOfBook { get; set; } = string.Empty;
+        [DisplayName("Book Author")]
         public string AuthorOfBook { get; set; } = string.Empty;
+        [DisplayName("Book Reliese Date")]
         public DateTime RelieseDateOfBook { get; set; }
+        [DisplayName("Book Rating")]
         public double RatingOfBook { get; set; }
     }
 }
