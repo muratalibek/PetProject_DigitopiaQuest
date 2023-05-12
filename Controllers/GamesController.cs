c8b6aac06c67aca4b1efdda78487e5fd661e82fa
muratBee01 <muratalibek01@gmail.com>
2023-05-12 15:59:57 +0600

    The CRUD configuration of an image property relative to the Games Model class.

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -56,10 +56,20 @@ namespace DigitopiaQuest.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,NameOfGame,DescriptionOfGame,UserScore,ReleaseDateOfGame,GenreOfGame,Developer,DevCompany")] Game game)
+        public async Task<IActionResult> Create(IFormFile imageFile, [Bind("Id,NameOfGame,DescriptionOfGame,UserScore,ReleaseDateOfGame,GenreOfGame,Developer,DevCompany,ImageOfMovie")] Game game)
         {
             if (ModelState.IsValid)
             {
+                if (imageFile != null && imageFile.Length > 0)// Explain!
+                {
+                    byte[] imageData;
+
+                    using (var binaryReader = new BinaryReader(imageFile.OpenReadStream()))
+                    {
+                        imageData = binaryReader.ReadBytes((int)imageFile.Length);
+                    }
+                    game.ImageOfMovie = imageData;
+                }
                 _context.Add(game);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -88,7 +98,7 @@ namespace DigitopiaQuest.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,NameOfGame,DescriptionOfGame,UserScore,ReleaseDateOfGame,GenreOfGame,Developer,DevCompany")] Game game)
+        public async Task<IActionResult> Edit(int id, IFormFile imageEdit, [Bind("Id,NameOfGame,DescriptionOfGame,UserScore,ReleaseDateOfGame,GenreOfGame,Developer,DevCompany,ImageOfMovie")] Game game)
         {
             if (id != game.Id)
             {
@@ -99,6 +109,16 @@ namespace DigitopiaQuest.Controllers
             {
                 try
                 {
+                    if (imageEdit != null && imageEdit.Length > 0)
+                    {
+                        byte[] imageData;
+
+                        using (var binaryReader = new BinaryReader(imageEdit.OpenReadStream()))
+                        {
+                            imageData = binaryReader.ReadBytes((int)imageEdit.Length);
+                        }
+                        game.ImageOfMovie = imageData;
+                    }
                     _context.Update(game);
                     await _context.SaveChangesAsync();
                 }
